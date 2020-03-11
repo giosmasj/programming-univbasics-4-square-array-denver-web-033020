a7b4c62acb40212ebd6e9d7cad8766aff5885a48
James Giosmas <giosmasj@gmail.com>
2020-03-11 03:52:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,8 @@ def square_array(array)
   counter = 0
   
   while array[counter] do
-    new array << array[counter] ** 2
+    new_array << array[counter] ** 2
     counter += 1
 end
+
+return new_array
